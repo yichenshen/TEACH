ff841d4388818e2d7fd8427c2eb9afe60ce1c46d
Shen Yichen <2007.yichen@gmail.com>
2015-05-19 16:40:33 +0800

    Add N-levels as level option
     - Fixes #6
    
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- source/models/level.model.php
+++ source/models/level.model.php
@@ -2,12 +2,14 @@
 	class Level
 	{
 		//Dummy data
-		static $levels = array(array("id" => 1,
-									"name" => "Primary"),
-							  array("id" => 2,
-							  		"name" => "O-Levels"),
-							  array("id" => 3,
-							  		"name" => "A-Levels"));
+		static $levels = array(	array("id" => 1,
+									  "name" => "Primary"),
+								array("id" => 2,
+									  "name" => "N-Levels"),
+								array("id" => 3,
+							  		  "name" => "O-Levels"),
+							  	array("id" => 4,
+							  		  "name" => "A-Levels"));
 
 		public static function all(){
 			return self::$levels;
--- source/pages/question/create.php
+++ source/pages/question/create.php
@@ -9,7 +9,7 @@
 
 	require $_SERVER['DOCUMENT_ROOT']."/models/level.model.php";
 	$levels = Level::all();
-	$defaultLevel = 2;
+	$defaultLevel = 3;
 
 	$mainContent = $_SERVER['DOCUMENT_ROOT']."/layouts/question/create.layout.php";
 	require($_SERVER['DOCUMENT_ROOT']."/layouts/shared/main.layout.php");
