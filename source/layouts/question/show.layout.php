1fb1fe717e9687d974d0d504684aa5cea07c170a
Shen Yichen <2007.yichen@gmail.com>
2015-05-23 12:03:51 +0800

    Add status and level labels in question view
     - resolves #4
    
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- source/layouts/question/show.layout.php
+++ source/layouts/question/show.layout.php
@@ -26,9 +26,13 @@
 	<div class="card blue-grey lighten-5">
 		<div class="card-content">
 		    <div class="section">
-			    <span class="hide-on-small-only">
-			    	<i class="grey-text text-darken-3 mdi-action-help small"></i>
-			    	&nbsp;
+		    	<div class="right hide-on-small-only">
+				    <div><?php echo label($question['status']); ?><br /></div>
+				    <div class="grey question-badge clear right"><?php echo $question["level"]; ?></div>
+			    </div>
+
+			    <span class="icon-wrapper hide-on-small-only">
+			    	<?php echo avatar($question["subject"]); ?>
 			    </span>
 			    	
 			    <span class="card-title black-text"><?php echo $question["title"]; ?></span>
