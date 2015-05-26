ec064be487bb170619707da80cb6fcda37ac8c00
Shen Yichen <2007.yichen@gmail.com>
2015-05-26 17:53:01 +0800

    Populate database
    Add data currently defined in website models.
    
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- db/schema.sql
+++ db/schema.sql
@@ -93,7 +93,7 @@ CREATE TABLE `questionLevelChanges` (
   CONSTRAINT `fk_level_changes_levels1` FOREIGN KEY (`initialLevelsId`) REFERENCES `levels` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
   CONSTRAINT `fk_level_changes_levels2` FOREIGN KEY (`finalLevelsId`) REFERENCES `levels` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
   CONSTRAINT `fk_level_changes_questions1` FOREIGN KEY (`questionId`) REFERENCES `questions` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -102,6 +102,7 @@ CREATE TABLE `questionLevelChanges` (
 
 LOCK TABLES `questionLevelChanges` WRITE;
 /*!40000 ALTER TABLE `questionLevelChanges` DISABLE KEYS */;
+INSERT INTO `questionLevelChanges` VALUES (1,1,'2015-05-26 09:39:16',NULL,1,1),(2,1,'2015-05-26 09:40:45',NULL,4,2),(3,1,'2015-05-26 09:41:40',4,3,2),(4,1,'2015-05-26 09:46:30',NULL,2,3),(5,1,'2015-05-26 09:46:30',NULL,3,4),(6,1,'2015-05-26 09:46:30',NULL,2,5),(7,0,'2015-05-26 09:46:30',2,4,5),(8,1,'2015-05-26 09:47:35',NULL,3,6);
 /*!40000 ALTER TABLE `questionLevelChanges` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -138,7 +139,7 @@ CREATE TABLE `questions` (
   CONSTRAINT `fk_questions_staff1` FOREIGN KEY (`staffUsername`) REFERENCES `staff` (`username`) ON DELETE SET NULL ON UPDATE CASCADE,
   CONSTRAINT `fk_questions_students` FOREIGN KEY (`studentsUsername`) REFERENCES `students` (`username`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `fk_questions_subjects1` FOREIGN KEY (`subjectsName`) REFERENCES `subjects` (`name`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -147,6 +148,7 @@ CREATE TABLE `questions` (
 
 LOCK TABLES `questions` WRITE;
 /*!40000 ALTER TABLE `questions` DISABLE KEYS */;
+INSERT INTO `questions` VALUES (1,'Why did the chicken cross the road?','I really can\'t think of a reason for it to do so!\\nHelp Me!','To get to the other side.',NULL,NULL,'2015-05-26 09:12:23','answered','2015-05-26 19:41:40',0,0,'user',NULL,1,'Biology'),(2,'What is 1+1?','Is it equals to the question ID of this question?','Very smart observation. \\n\\n Now stop wasting your money.',NULL,NULL,'2015-05-26 09:12:23','modified','2015-05-26 09:41:40',0,0,'user','staff',3,'Maths'),(3,'Can I ask a question?','It\'s a really interesting question!\\n\\nI really want to ask it!',NULL,NULL,NULL,'2015-05-26 09:22:04','open','2015-05-26 09:22:04',0,0,'user',NULL,2,'Chemistry'),(4,'This is a long and really really really really really really really really really old question.','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur porttitor ut ex quis posuere. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque lobortis dapibus ullamcorper. Curabitur sagittis sapien in nisl ornare sodales. Sed sodales erat in arcu maximus, id ultrices risus scelerisque. Etiam dictum dolor mi, eu finibus nisi tristique et. Suspendisse orci massa, fermentum rhoncus dolor vel, gravida tempus odio. Suspendisse ut arcu efficitur, dignissim massa ut, congue sem. Ut et dolor urna. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris faucibus aliquet nibh sed cursus. Integer lacinia scelerisque dui, interdum viverra odio rutrum vitae. Vivamus quis eros pretium, vehicula orci nec, cursus nisl. Phasellus ac imperdiet lorem. Maecenas luctus purus id congue vulputate. \\n\\nMorbi placerat elit ac diam eleifend imperdiet. Integer a cursus felis, vel lobortis ante. Donec commodo facilisis fermentum. Vestibulum ut augue faucibus, euismod risus ut, volutpat ligula. Proin eu ullamcorper mi. Nulla sit amet pretium orci, vel pretium nisi. Maecenas id elit iaculis, tempus lectus id, convallis felis. Suspendisse mollis mi lectus, ut gravida leo condimentum quis. Nunc vehicula libero vitae ipsum placerat volutpat. Pellentesque id sem a arcu tristique gravida non eu eros. Phasellus ultrices, lectus eget mollis elementum, neque mauris ullamcorper massa, id tempor orci diam ut sem. Sed placerat tortor et ornare ultrices. Duis interdum neque non sagittis tempus. Quisque quis euismod sem.\\n\\nPellentesque id erat nisi. Sed et felis tellus. Cras consequat neque mauris, eu mollis risus pulvinar quis. Aliquam vitae viverra ligula. Fusce interdum ipsum urna, et blandit tellus malesuada quis. In ac tortor nisl. Maecenas id dolor scelerisque, pulvinar ligula at, gravida magna. Sed mauris lorem, pretium ac aliquet vel, laoreet vitae risus. Vivamus porta enim sit amet.','Uh I\'m really sorry but can you repeat that again?',NULL,NULL,'2015-05-26 09:22:04','read','2015-05-27 09:39:31',0,0,'Jack','staff',3,NULL),(5,'Zombie cat?','Schrödinger wrote:\\n\\n A cat is penned up in a steel chamber, along with the following device (which must be secured against direct interference by the cat): in a Geiger counter, there is a tiny bit of radioactive substance, so small, that perhaps in the course of the hour one of the atoms decays, but also, with equal probability, perhaps none; if it happens, the counter tube discharges and through a relay releases a hammer that shatters a small flask of hydrocyanic acid. If one has left this entire system to itself for an hour, one would say that the cat still lives if meanwhile no atom has decayed. The psi-function of the entire system would express this by having in it the living and dead cat (pardon the expression) mixed or smeared out in equal parts.\\n\\nSo is the cat alive or dead?','There are many explanations to this question.\\n\\nThe most popular of which is the Copenhagen interpretation, which states that the system will cease to be in a superposition of states upon observation. This implies that until you open the box (or find out for certain if the cat is alive or dead), the cat is indeed, both dead and alive. (Zombie cat ftw)\\n\\nThe other popular interpretation is the many worlds interpretation. In this case, the cat is alive in one world and dead in the other.',NULL,NULL,'2015-05-26 09:25:41','fined','2015-05-26 09:46:30',0,0,'user',NULL,4,'Physics'),(6,'CuSO4','What is the molar mass (M) of Copper(II) Sulphate (CuSO4)?',NULL,NULL,NULL,'2015-05-26 09:25:41','open','2015-05-26 09:25:41',0,0,'Jack','staff',3,'Chemistry');
 /*!40000 ALTER TABLE `questions` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -175,6 +177,7 @@ CREATE TABLE `staff` (
 
 LOCK TABLES `staff` WRITE;
 /*!40000 ALTER TABLE `staff` DISABLE KEYS */;
+INSERT INTO `staff` VALUES ('staff','staff@teach.com','password','Full-Time','0000-00-00 00:00:00');
 /*!40000 ALTER TABLE `staff` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -201,6 +204,7 @@ CREATE TABLE `staffHasSubjects` (
 
 LOCK TABLES `staffHasSubjects` WRITE;
 /*!40000 ALTER TABLE `staffHasSubjects` DISABLE KEYS */;
+INSERT INTO `staffHasSubjects` VALUES ('Chemistry','staff'),('Maths','staff'),('Physics','staff');
 /*!40000 ALTER TABLE `staffHasSubjects` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -233,6 +237,7 @@ CREATE TABLE `students` (
 
 LOCK TABLES `students` WRITE;
 /*!40000 ALTER TABLE `students` DISABLE KEYS */;
+INSERT INTO `students` VALUES ('Jack','jack@hotmail.com','jackjack',100.00,0.00,'2015-05-26 08:51:34',3),('user','user@teach.com','password',200.00,1.00,'2015-05-20 05:42:10',4);
 /*!40000 ALTER TABLE `students` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -299,4 +304,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-05-26 13:30:19
+-- Dump completed on 2015-05-26 17:51:33
