02f0d0cb856a62266ee29992a7c16766fa5c8c44
Shen Yichen <2007.yichen@gmail.com>
2015-06-01 22:19:56 +0800

    Update index page
    Added in introductions for signups and contact.
    
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- source/layouts/index.layout.php
+++ source/layouts/index.layout.php
@@ -1,13 +1,72 @@
 <div class="container">
   <div class="center">    
     <h1>Welcome to TEACH</h1>
+    
     <div class="row">
       <div class="col s6 offset-s3">
         <img src="/resources/images/school1.svg">
       </div>
     </div>
-    <center>
-    	<a class="waves-effect waves-light btn-large light-blue darken-1" href="/pages/account/signup.php">Sign-up Now</a>
-    </center>
+      
+    <div class="divider"></div>
+
+    <div class="row section">
+      <div class="col s12 m5">
+          
+          <div class="row">
+            <div class="col offset-s3 s6">
+              <img src="/resources/images/general.svg" style="width:100%">
+            </div>
+          </div>
+
+          <h4>Signup today!</h4>
+
+          <div class="left-align">
+            <p>
+              Looking for help with your studies? Look no further!
+            </p>
+
+            <p>
+              Signup with us now and you'll never have another unanswered question again! Here at TEACH we are committed to answering your academic queries. 
+              Ask any question and pay for only what you need and nothing else. Come back and check out the answers anytime, anywhere.
+            </p>
+            
+            <p>
+              Our team of decicated and certified tutors will provide you with clear and concise answers, tailored to the Singapore exam format.
+              Say goodbye to all that confusing and conflicting mess on the Internet!
+            </p>
+          </div>
+
+          <a class="waves-effect waves-light btn-large light-blue darken-1" href="/pages/account/signup.php">Sign-up Now</a>
+      </div>
+
+      <div class="col s12 offset-m2 m5">
+         <div class="row">
+            <div class="col offset-s3 s6">
+              <img src="/resources/images/network.svg" style="width:100%">
+            </div>
+          </div>
+
+          <h4>Join our Family!</h4>
+
+          <div class="left-align">
+            <p>
+              Interested in teaching? Have some free time?
+            </p>
+            <p>
+              As a social enterprise we are dedicated to improve educations in Singapore and we welcome you to join us for our journey!
+            </p>
+            <p>
+              You do not need to commit full time to be part of us. As a part-time teaching member, you can choose to help out as and when you like.
+              We are also happy to accept any form of help from anyone who believes in our cause as a social enterprise. 
+            </p>
+            <p>
+              Contact us below for more details.
+            </p>
+          </div>
+
+          <a class="waves-effect waves-light btn-large light-blue darken-1" href="#">Contact Us</a>
+      </div>
+    </div>
   </div>
 </div>
