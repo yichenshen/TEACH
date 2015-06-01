da5b87471a6dab5a4b921123240b2c72752c1d96
Shen Yichen <2007.yichen@gmail.com>
2015-06-01 15:12:06 +0800

    Add model for service level
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

---
+++ source/models/service.model.php
@@ -0,0 +1,30 @@
+<?php
+	class ServiceLevel
+	{
+		static $serviceLevels = array(	array(	"id" => 1,
+												"name" => "Normal",
+												"costMultiplier" => 1,
+												"answerTime" => "72:00:00"),
+										array(	"id" => 2,
+												"name" => "Premium",
+												"costMultiplier" => 1.5,
+												"answerTime" => "24:00:00"),
+										array(	"id" => 2,
+												"name" => "Premium",
+												"costMultiplier" => 1.5,
+												"answerTime" => "24:00:00"));
+
+		public static function all(){
+			return self::$serviceLevels;
+		}
+
+		public static function getServiceLevel($id){
+			foreach (self::$serviceLevels as $sl) {
+				if ($sl["id"] == $id) {
+					return $sl;
+				}
+			}
+
+			return null;
+		}
+	}
